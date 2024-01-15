c841c9a48bf6e371d8a7917a890c0acf2811d2a4
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-14 22:19:26 -0500

    improved speed with an algorithm that merges by comparing and inserting elements from their ends towards the beginning in C++

--- MergeSortedArray/MergeSortedArray.cpp
+++ MergeSortedArray/MergeSortedArray.cpp
@@ -1,16 +1,29 @@
 #include <vector>
-#include <algorithm> // For std::sort
 using namespace std;
 
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Copy nums2 to the end of nums1
-        for (int i = 0; i < n; ++i) {
-            nums1[m + i] = nums2[i];
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
         }
 
-        // Now sort the entire nums1 array
-        sort(nums1.begin(), nums1.end());
+        // Copy any remaining elements from nums2
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
     }
 };
