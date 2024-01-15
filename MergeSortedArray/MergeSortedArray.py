d7c77912401645ec00c43b825ad8edb342f19a81
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-14 22:22:50 -0500

    Made the improved solution in python aswell.

--- MergeSortedArray/MergeSortedArray.py
+++ MergeSortedArray/MergeSortedArray.py
@@ -1,18 +1,23 @@
 class Solution:
     def merge(self, nums1, m, nums2, n):
-        # Copy nums2 to the end of nums1
-        for i in range(n):
-            nums1[m + i] = nums2[i]
+        p1, p2, p = m - 1, n - 1, m + n - 1
 
-        # Now sort the entire nums1 array
-        nums1.sort()
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
 
-        return nums1
+        # Copy any remaining elements from nums2
+        nums1[:p2 + 1] = nums2[:p2 + 1]
 
-# Test the method with the provided examples
-sol = Solution()
-example1 = sol.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
-example2 = sol.merge([1], 1, [], 0)
-example3 = sol.merge([0], 0, [1], 1)
+# Example usage
+solution = Solution()
+example1 = solution.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+example2 = solution.merge([1], 1, [], 0)
+example3 = solution.merge([0], 0, [1], 1)
 
 example1, example2, example3
